922bcb4dc71b048bd8766f452aeef067ffa4f07f
OscarLlanos1412 <ojllanos25@misena.edu.co>
2021-12-20 20:57:43 -0500

    Agregado un try catch para que no se digite letras

--- TrianguloPascal/Program.cs
+++ TrianguloPascal/Program.cs
@@ -6,49 +6,56 @@ namespace TrianguloPascal
     {
         static void Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.Green;
-            int bloques = 0;
-            //ARREGLO PRINCIPAL
-            int[] arreglo = new int[1];
-            Console.WriteLine("Ingrese el numero de bloques: ");
-            bloques = Convert.ToInt16(Console.ReadLine());
-
-            //Se recorre la vble bloque que es el tamaño del triangulo
-            for (int i = 1; i <= bloques; i++)
+            try
             {
-                //Se crea el arreglo pascal para 
-                int[] pascal = new int[i];
+                Console.ForegroundColor = ConsoleColor.Green;
+                int bloques = 0;
+                //ARREGLO PRINCIPAL
+                int[] arreglo = new int[1];
+                Console.WriteLine("Ingrese el numero de bloques: ");
+                bloques = Convert.ToInt16(Console.ReadLine());
 
-                //Genera espacios de acuerdo el tamaño del triangulo
-                for (int j = bloques; j > i; j--)
+                //Se recorre la vble bloque que es el tamaño del triangulo
+                for (int i = 1; i <= bloques; i++)
                 {
-                    Console.Write(" ");
-                }
-                
-                //
-                for (int k = 0; k < i; k++)
-                {
-                    /*Comprueba si la vble k es igual a 0 y agrega al arreglo
-                     de lo contrario este sumara el valor de una posicion con otra
-                    e imprime el triangulo*/
-                    if (k == 0 || k == (i - 1))
-                    {
-                        pascal[k] = 1;
-                    }
-                    else
-                    {
-                        pascal[k] = arreglo[k] + arreglo[k - 1]; 
-                    }
-                    //Impresión de partes del triangúlo
-                    Console.Write("[" + pascal[k] + "]");
-                }
-                //Agrega los valores al arreglo principal 
-                arreglo = pascal;
+                    //Se crea el arreglo pascal para 
+                    int[] pascal = new int[i];
 
-                //Genera los saltos de linea cada vez que imprime un bloque
-                Console.WriteLine("");
+                    //Genera espacios de acuerdo el tamaño del triangulo
+                    for (int j = bloques; j > i; j--)
+                    {
+                        Console.Write(" ");
+                    }
+
+                    //
+                    for (int k = 0; k < i; k++)
+                    {
+                        /*Comprueba si la vble k es igual a 0 y agrega al arreglo
+                         de lo contrario este sumara el valor de una posicion con otra
+                        e imprime el triangulo*/
+                        if (k == 0 || k == (i - 1))
+                        {
+                            pascal[k] = 1;
+                        }
+                        else
+                        {
+                            pascal[k] = arreglo[k] + arreglo[k - 1];
+                        }
+                        //Impresión de partes del triangúlo
+                        Console.Write("[" + pascal[k] + "]");
+                    }
+                    //Agrega los valores al arreglo principal 
+                    arreglo = pascal;
+
+                    //Genera los saltos de linea cada vez que imprime un bloque
+                    Console.WriteLine("");
+                }
+                Console.ReadLine();
             }
-            Console.ReadLine();
+            catch (FormatException)
+            {
+                Console.WriteLine("No puede digitar letras");
+            }
         }
     }
 }
